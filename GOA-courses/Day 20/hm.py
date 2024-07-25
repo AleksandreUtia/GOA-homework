696fc0fae27b76a2b99b6cba95e7dc0ef6dea838
AleksandreUtia <leqso.utiashvili1@gmail.com>
2024-07-25 16:50:02 +0400

    Add files via upload

---
+++ GOA-courses/Day 20/hm.py
@@ -0,0 +1,5 @@
+for i in range(6):
+    if i == 7: break
+    print(i)
+else:
+    print("finally finished!")
---
+++ GOA-courses/Day 20/main.py
@@ -0,0 +1,15 @@
+# def print_number():
+#     for i in range(1, 20 + 1):
+#         print(i)
+# print_number()
+
+
+for i in range(1, 20 + 1):
+    print(i)
+#შევქმენით ფორ ფუნქცია სადაც გამოვატანინეთ 1 დან 20ის ჩთვლით რიცხვები 
+    
+def numbers(start, end):
+    for number in range(start, end + 1):
+        print(number)
+
+        
